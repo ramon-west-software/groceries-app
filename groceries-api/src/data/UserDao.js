1f39a9d32e0a20bd5d009cd9858b3cc812f4862d
ramonwest <ramonwest613@gmail.com>
2023-11-01 20:49:43 -0700

    reformat node server response

--- groceries-api/src/data/UserDao.js
+++ groceries-api/src/data/UserDao.js
@@ -1,15 +1,12 @@
-// import ConnectionRequest from "./ConnectionRequest.js";
-
-import {
-  selectData,
-  selectDataWithParms,
-  insertData,
-} from "./ConnectionRequest.js";
+import { selectData, selectDataWithParms, insertData } from "./Database.js";
+import GroceryItem from "../model/GroceryItem.js";
+import Category from "../model/Category.js";
+import StorageArea from "../model/StorageArea.js";
 
 class UserDao {
-
-
+  // set sql query and call Database.js query function
   async getUserData(id) {
+    // define query string
     const sql =
       "SELECT sa.storage_id, sa.storage_name, c.category_id, c.category_name, gi.grocery_item_id, gi.name, gi.item_duration, gi.purchase_date " +
       "FROM users u " +
@@ -20,16 +17,64 @@ class UserDao {
       "LEFT JOIN category_grocery_items cgi ON c.category_id = cgi.category_id " +
       "LEFT JOIN grocery_items gi ON cgi.grocery_item_id = gi.grocery_item_id " +
       "WHERE u.user_id = ?" +
-      "group by sa.storage_id, sa.storage_name, c.category_id, c.category_name, gi.grocery_item_id, gi.name, gi.item_duration, gi.purchase_date";
+      "GROUP BY sa.storage_id, sa.storage_name, c.category_id, c.category_name, gi.grocery_item_id, gi.name, gi.item_duration, gi.purchase_date";
 
-    const userData = await selectDataWithParms(sql, [id]);
-    
+    // call async Database.js function to query the database
+    let userData = await selectDataWithParms(sql, [id]);
+
+    // format the data returned by Database
+    userData = formatResults(userData);
+
+    // return formatted data
     return userData;
   }
 
   async getAllUsers() {
     return "all users";
   }
+}
+
+// helper function to format query results
+// TODO: is this function private to this file if not exported??
+function formatResults(data) {
+  console.log("private helper function");
+
+  const storageAreas = {};
+
+  // for each row/item in the result array
+  data
+  .filter(item => item.category_id !== null) 
+  .forEach((item) => {
+    const storageId = item.storage_id;
+    const categoryId = item.category_id;
+
+    if (!storageAreas[storageId]) {
+      storageAreas[storageId] = new StorageArea(storageId, item.storage_name);
+    }
+
+    const storage = storageAreas[storageId];
+
+    if (!storage.categories[categoryId]) {
+      storage.categories[categoryId] = new Category(
+        categoryId,
+        item.category_name
+      );
+    }
+
+    const category = storage.categories[categoryId];
+
+    const groceryItem = new GroceryItem(
+      item.grocery_item_id,
+      item.name,
+      item.item_duration,
+      item.purchase_date
+    );
+
+    category.addItem(groceryItem);
+  });
+
+  const result = Object.values(storageAreas);
+  return result;
 }
 
 export default UserDao;
